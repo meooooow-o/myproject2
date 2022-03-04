05869c6a8a4a5a5f50ad89fb211f2c9b96275180
Meooooow <410570126@gapp.fju.edu.tw>
2022-03-04 15:55:14 +0800

    scoring testing2

--- src/main/java/com/meow/ScoringNG.java
+++ src/main/java/com/meow/ScoringNG.java
@@ -1,12 +1,16 @@
 package com.meow;
 
+import java.sql.SQLOutput;
+
 public class ScoringNG {
     public static void main(String[] args) {
-        String[] name = {"Alice","Joyce","Peggy"};
-        int[] english = {70,80,96};
-        int[] math = {50,60,98};
-        for (int i = 0; i < 3; i++) {
-            System.out.println(name[i] + "\t" + english[i] + "\t" + math[i] + "\t" + (english[i]+math[i])/2);
+        String[] name = {"Alice","Joyce","Peggy","Lisa","Iris"};
+        int[] english = {70,80,96,85,100};
+        int[] math = {50,60,98,60,100};
+        for (int i = 0; i < 5; i++) {
+            if (i != 3) {
+                System.out.println(name[i] + "\t" + english[i] + "\t" + math[i] + "\t" + (english[i] + math[i]) / 2);
+            }
         }
     }
 }
