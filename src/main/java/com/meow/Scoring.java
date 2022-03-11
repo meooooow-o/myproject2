4445e7da93207c2d646c499a5cf1635613e02754
meooooow <410570126@gapp.fju.edu.tw>
2022-03-11 14:28:12 +0800

    建構值，內有兩種方法來表示

--- src/main/java/com/meow/Scoring.java
+++ src/main/java/com/meow/Scoring.java
@@ -2,9 +2,14 @@ package com.meow;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student jack = new Student();
+        Student jack = new Student("Jack");
         jack.name = "Jack";
         jack.english = 70;
         jack.math = 80;
+        jack.print();
+
+        Student hank = new Student("Hank",80,60);
+        hank.print();
+
     }
 }
--- src/main/java/com/meow/Student.java
+++ src/main/java/com/meow/Student.java
@@ -4,4 +4,21 @@ public class Student {
     String name;
     int english;
     int math;
+    //建構特別方法，建構值，需和Class為同名稱，且無回傳值
+    public Student(String name){
+        this.name = name; //需顏色不同，使用this來表示非方法內的name，而是int name
+
+        //super();
+    }
+    //第二種建構方法，特別方法可以再內部產生建構值，須注意種類及屬性
+    public Student(String name, int english , int math){
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+
+    //建構void
+    public void print(){
+        System.out.println(name + "\t" + english + "\t" + math);
+    }
 }
