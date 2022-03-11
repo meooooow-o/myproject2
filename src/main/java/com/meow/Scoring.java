08fe3e08f6529a128ab2df4986823af265b5d6c2
meooooow <410570126@gapp.fju.edu.tw>
2022-03-11 15:48:32 +0800

    建構值

--- src/main/java/com/meow/Scoring.java
+++ src/main/java/com/meow/Scoring.java
@@ -2,13 +2,10 @@ package com.meow;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student jack = new Student("Jack");
-        jack.name = "Jack";
-        jack.english = 70;
-        jack.math = 80;
-        jack.print();
-
+        Student jack = new Student("Jack",50,42);
         Student hank = new Student("Hank",80,60);
+
+        jack.print();
         hank.print();
 
     }
--- src/main/java/com/meow/Student.java
+++ src/main/java/com/meow/Student.java
@@ -4,21 +4,31 @@ public class Student {
     String name;
     int english;
     int math;
-    //建構特別方法，建構值，需和Class為同名稱，且無回傳值
-    public Student(String name){
-        this.name = name; //需顏色不同，使用this來表示非方法內的name，而是int name
+    
 
-        //super();
-    }
+    //建構特別方法，建構值，需和Class為同名稱，且無回傳值
     //第二種建構方法，特別方法可以再內部產生建構值，須注意種類及屬性
     public Student(String name, int english , int math){
+//        this(name);
         this.name = name;
         this.english = english;
         this.math = math;
     }
 
+    public Student(){
+        this("Jhon Doe",-1,-1);
+        /*name = "Jhon Doe";
+        english = -1;
+        math = -1;*/
+    }
+
     //建構void
     public void print(){
-        System.out.println(name + "\t" + english + "\t" + math);
+        int average = (english+math)/2;
+        System.out.print(name + "\t" + english + "\t" + math + "\t");
+
+        if (average<60)
+            System.out.print("*");
+        System.out.println();
     }
 }
